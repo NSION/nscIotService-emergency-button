a85dd6ed0a142af6d64c5adbcb952c5d9554b639
Jukka Raivio <juraivio@gmail.com>
2021-01-31 22:27:36 +0200

    First early draft

---
+++ nsc-emergency-button.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+import requests
+
+BtnPin1 = 23    # pin23 --- button1
+
+def setup():
+    GPIO.setmode(GPIO.BCM)       # Numbers GPIOs by physical location
+    GPIO.setup(BtnPin1, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
+    GPIO.setwarnings(False)
+
+def switchon(ev=None):
+    if GPIO.input(BtnPin1):     # if port BtnPin1 == 1  
+        print('Broadcasting off\r')
+        requests.post('http://localhost:8091/nscIotService/media/live/end')  
+    else: 
+        print('Broadcasting on\r')
+        requests.post('http://localhost:8091/nscIotService/media/live/start')
+def loop():
+    GPIO.add_event_detect(BtnPin1, GPIO.FALLING, callback=switchon, bouncetime=300)
+    while True:
+        pass   
+def destroy():
+        GPIO.cleanup() 
+
+if __name__ == '__main__':     # Program start from here
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+        destroy()
