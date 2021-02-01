f57c955ac94021e851b7ed92486b4840648f08ef
Jukka Raivio <juraivio@gmail.com>
2021-02-01 09:00:10 +0200

    Update nsc-emergency-button.py

--- nsc-emergency-button.py
+++ nsc-emergency-button.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import time
 import requests
 
-BtnPin1 = 23    # pin23 --- button1
+BtnPin1 = 4    # pin4 --- button1
 
 def setup():
     GPIO.setmode(GPIO.BCM)       # GPIO Layoyt BCM
@@ -12,12 +12,12 @@ def setup():
 
 def switchon(ev=None):
     if GPIO.input(BtnPin1):     # if port BtnPin1 == 1  
-        print('Broadcasting off\r')
-        requests.post('http://localhost:8091/nscIotService/media/live/end')  
-    else: 
         print('Broadcasting on\r')
         requests.post('http://localhost:8091/nscIotService/media/live/start')
-
+    else: 
+        print('Broadcasting off\r')
+        requests.post('http://localhost:8091/nscIotService/media/live/end')  
+        
 def loop():
     GPIO.add_event_detect(BtnPin1, GPIO.FALLING, callback=switchon, bouncetime=250)
     while True:
